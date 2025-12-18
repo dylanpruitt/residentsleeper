19c7e25ac602f309507e05145e03c763d0cd2dba
Dylan Pruitt <dpruitt0703@gmail.com>
2025-12-18 17:14:31 -0500

    feat(server/main.go): added simple API mock with different versions

--- server/main.go
+++ server/main.go
@@ -25,6 +25,74 @@ func headers(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, string(headerResponse))
 }
 
+func mockAPI(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	apiVersion := req.Header.Get("Accept")
+	if apiVersion == "application/json;v=1" {
+		data := getMockAPIData(req.PathValue("key"), 1)
+		if data != nil {
+			w.WriteHeader(http.StatusOK)
+			apiResponse, _ := json.Marshal(&data)
+			fmt.Fprintf(w, string(apiResponse))
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	} else if apiVersion == "application/json;v=2" {
+		data := getMockAPIData(req.PathValue("key"), 2)
+		if data != nil {
+			w.WriteHeader(http.StatusOK)
+			fmt.Println(data)
+			apiResponse, _ := json.Marshal(&data)
+			fmt.Println(string(apiResponse))
+			fmt.Fprintf(w, string(apiResponse))
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		apiResponse, _ := json.Marshal("API version not supported. Must pass Accept header set to application/json;v=1 OR application/json;v=2")
+		fmt.Fprintf(w, string(apiResponse))
+	}
+}
+
+type V1Data struct {
+	Name    string    `json:"name"`
+	Account V1Account `json:"account"`
+}
+
+type V1Account struct {
+	Money int `json:"money"`
+}
+
+type V2Data struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+
+	Money int `json:"money"`
+}
+
+func getMockAPIData(key string, apiVersion int) interface{} {
+	if key == "1" {
+		if apiVersion == 1 {
+			return V1Data{
+				Name: "John Wick",
+				Account: V1Account{
+					Money: 100000,
+				},
+			}
+		} else {
+			return V2Data{
+				FirstName: "John",
+				LastName:  "Wick",
+				Money:     100000,
+			}
+		}
+	} else {
+		return nil
+	}
+}
+
 func return404(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 }
@@ -34,11 +102,11 @@ func return500(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
 	http.HandleFunc("/4xxtest", return404)
 	http.HandleFunc("/5xxtest", return500)
+	http.HandleFunc("/user/{key}", mockAPI)
 
 	http.ListenAndServe(":8090", nil)
 }
